95bb2923c8b1c0e8d1468ffd7cc8435748b5f299
aidenpc23 <aidenpc23@gmail.com>
2023-06-08 17:56:12 -0400

    Start of the new Manokeso 2 engine...

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,52 @@
-use bevy::{
-    prelude::*,
-    render::render_resource::PrimitiveTopology,
-    sprite::MaterialMesh2dBundle,
-    render::mesh::Indices,
+use state::State;
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{WindowBuilder},
 };
-
-pub const CLEAR: Color =  Color::rgb(0.1, 0.1, 0.1);
-
+ 
+mod state;
+mod render;
+ 
 fn main() {
-    App::new()
-        .insert_resource(ClearColor(CLEAR))
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .run();
+    pollster::block_on(run());
 }
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let size: f32 = 50.0;
-    
-    // Arbitrary points for the quad
-    let p1 = [0.0, 0.0, 0.0];
-    let p2 = [size, 0.0, 0.0];
-    let p3 = [size, size, 0.0];
-    let p4 = [0.0, size, 0.0];
-
-    let mesh = create_quad(p1, p2, p3, p4);
-
-    commands.spawn( MaterialMesh2dBundle  {
-        mesh: meshes.add(mesh).into(),
-        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
-        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
-        ..default()
+ 
+async fn run() {
+    // Setup logger
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    // Create window and application state
+    let window = WindowBuilder::new()
+        .with_visible(false)
+        .build(&event_loop)
+        .unwrap();
+ 
+    let mut state: State = State::new(window).await;
+ 
+    // ==============================================
+    // Define event loop functionality
+    // ==============================================
+    state.window.set_visible(true);
+ 
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+ 
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == state.window.id() => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent {
+                event: WindowEvent::KeyboardInput { input, .. },
+                window_id,
+            } if window_id == state.window.id() => {
+                if input.virtual_keycode == Some(VirtualKeyCode::Escape) {
+                    *control_flow = ControlFlow::Exit
+                }
+            }
+            Event::RedrawRequested(_) => state.render(),
+            _ => (),
+        }
     });
-    
-    commands.spawn(Camera2dBundle::default());
-}
-
-/// Creates a 2D quadrilateral mesh from four specified points.
-///
-/// This function takes four points in 3D space, each represented as a 3-element array of `f32`, 
-/// and uses them to create a 2D quadrilateral mesh.
-/// 
-/// # Returns
-///
-/// A `Mesh` object representing a quadrilateral with the specified vertices. The mesh has a `TriangleList` topology, 
-/// meaning it's made of two triangles defined by the vertex indices `[0, 1, 2]` and `[0, 2, 3]`.
-///
-/// # Example
-///
-/// ```rust
-/// let p1 = [0.0, 0.0, 0.0];
-/// let p2 = [1.0, 0.0, 0.0];
-/// let p3 = [1.0, 1.0, 0.0];
-/// let p4 = [0.0, 1.0, 0.0];
-/// let quad_mesh = create_quad(p1, p2, p3, p4);
-/// ```
-fn create_quad(p1: [f32; 3], p2: [f32; 3], p3: [f32; 3], p4: [f32; 3]) -> Mesh {
-    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
-    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vec![p1, p2, p3, p4]);
-    mesh.set_indices(Some(Indices::U32(vec![0, 1, 2, 0, 2, 3])));
-    mesh
 }
---
+++ src/render.rs
@@ -0,0 +1,82 @@
+use wgpu::{CommandEncoder, TextureView, RenderPass};
+ 
+use crate::state::{CLEAR_COLOR, State};
+ 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+ 
+impl Vertex {
+    const ATTRIBS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+ 
+    pub fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+ 
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS,
+        }
+    }
+}
+ 
+pub const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.5, -0.5, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [0.5, 0.5, 0.0], color: [0.0, 0.0, 1.0] },
+    Vertex { position: [-0.5, 0.5, 0.0], color: [0.0, 1.0, 0.0] },
+];
+ 
+pub const INDICES: &[u16] = &[
+    0, 1, 2,
+    0, 2, 3,
+];
+ 
+impl State {
+    pub fn render(&mut self) {
+        let output = self.surface.get_current_texture().unwrap();
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+ 
+        self.draw(&mut encoder, &view);
+ 
+        // submit will accept anything that implements IntoIter
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+    }
+ 
+    /// Function: draw
+    ///
+    /// Summary:
+    ///  Every frame of the game draws to the screen.
+    ///
+    fn draw(&mut self, encoder: &mut CommandEncoder, view: &TextureView) {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(CLEAR_COLOR),
+                    store: true,
+                },
+            })],
+            depth_stencil_attachment: None,
+        });
+        render_pass.set_pipeline(&self.render_pipeline);
+ 
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+        render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+        render_pass.draw_indexed(0..6, 0, 0..1);
+    }
+}
