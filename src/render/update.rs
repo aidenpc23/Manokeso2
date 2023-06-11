8e3e2837a3ae81760f6214181517d033db3ffffd
Bryan McShea <cat71602@gmail.com>
2023-06-10 20:09:02 -0400

    make culling sane and add more chunks

--- src/render/update.rs
+++ src/render/update.rs
@@ -1,11 +1,10 @@
-use std::time;
-
 use wgpu::util::DeviceExt;
 
 use crate::state::GameState;
 
-const CHUNK_SHIFT: u32 = 5;
-const CHUNK_SIZE: usize = 2usize.pow(CHUNK_SHIFT);
+const CHUNK_ALIGN: u32 = 5;
+const CHUNK_SIZE: i32 = 2i32.pow(CHUNK_ALIGN);
+const CHUNK_MASK: i32 = !(CHUNK_SIZE-1);
 
 use super::{
     uniform::{CameraUniform, TileViewUniform},
@@ -18,19 +17,29 @@ impl Renderer {
         let size = &self.window.inner_size();
         let uniform = CameraUniform::new(camera, size);
 
-        let [xs, ys] = self.uniforms.camera.bottom_left();
-        let [xe, ye] = self.uniforms.camera.top_right();
-        let [bx, by] = [0.0, 0.0];
-        let bxe = state.board.width();
-        let bye = state.board.height();
-        let mut xs = f32::max(xs - bx + 0.5, 0.0) as usize;
-        let mut ys = f32::max(ys - by + 0.5, 0.0) as usize;
-        let mut xe = f32::max(xe - bx + 1.5, xs as f32) as usize;
-        let mut ye = f32::max(ye - by + 1.5, ys as f32) as usize;
-        xs = usize::min((xs >> CHUNK_SHIFT) << CHUNK_SHIFT, bxe);
-        ys = usize::min((ys >> CHUNK_SHIFT) << CHUNK_SHIFT, bye);
-        xe = usize::min(((xe >> CHUNK_SHIFT) << CHUNK_SHIFT) + CHUNK_SIZE, bxe);
-        ye = usize::min(((ye >> CHUNK_SHIFT) << CHUNK_SHIFT) + CHUNK_SIZE, bye);
+        // get positions in the world
+        // s = start, e = end
+        let [cxs, cys] = self.uniforms.camera.bottom_left();
+        let [cxe, cye] = self.uniforms.camera.top_right();
+        let [bx, by] = [0.0, 0.0]; // TODO: get board x and y from state
+        let bw = state.board.width();
+        let bh = state.board.height();
+        // calculate camera position relative to board position
+        // also account for tile mesh (-.5 -> .5)
+        let xs = (cxs - bx + 0.5) as i32;
+        let ys = (cys - by + 0.5) as i32;
+        let xe = (cxe - bx + 1.5) as i32;
+        let ye = (cye - by + 1.5) as i32;
+        // align with chunks and add an extra chunk in each direction
+        let xs = (xs & CHUNK_MASK) - CHUNK_SIZE;
+        let ys = (ys & CHUNK_MASK) - CHUNK_SIZE;
+        let xe = (xe & CHUNK_MASK) + 2 * CHUNK_SIZE;
+        let ye = (ye & CHUNK_MASK) + 2 * CHUNK_SIZE;
+        // cut off values for bounds
+        let xs = (xs.max(0) as usize).min(bw);
+        let ys = (ys.max(0) as usize).min(bh);
+        let xe = (xe.max(0) as usize).min(bw);
+        let ye = (ye.max(0) as usize).min(bh);
 
         let old_len = self.instances.len();
         self.instances = state.board.render_attributes(xs, xe, ys, ye);
